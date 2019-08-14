bfe6b6483ee9ce31aa7eb35780d4508ab6afbdb6
Nano <ginocarlucci@hotmail.com>
2019-08-14 19:13:04 -0300

    Agrego logic Especialidad y moduloUsuario

---
+++ Business.Logic/ModuloUsuarioLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Database;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    class ModuloUsuarioLogic
+    {
+        private Data.Database.ModuloUsuarioAdapter _ModuloUsuarioData;
+
+        public ModuloUsuarioLogic()
+        {
+            _ModuloUsuarioData = new ModuloUsuarioAdapter();
+        }
+        public ModuloUsuario GetOne(int ID)
+        {
+            return _ModuloUsuarioData.GetOne(ID);
+        }
+        public List<ModuloUsuario> GetAll()
+        {
+            return _ModuloUsuarioData.GetAll();
+        }
+        public void Save(ModuloUsuario moduloUsuario)
+        {
+            _ModuloUsuarioData.Save(moduloUsuario);
+        }
+        public void Delete(int ID)
+        {
+            _ModuloUsuarioData.Delete(ID);
+        }
+    }
+}
